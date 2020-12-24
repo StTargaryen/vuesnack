455aff6227502e31aae5be66375f027a2e855794
Andrey Peshko <a.peshko@smartleads.team>
2020-12-24 10:51:39 +0300

    First stable non-styled snack

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import snack from "./snack";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    snack,
+  },
+});
